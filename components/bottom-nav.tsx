a1eb2625c8630bed8c916769e7835e907f3946fa
Mo-Jain-back <jainmohit1199@outlook.com>
2025-03-02 19:05:51 +0530

    fixing_bg

--- components/bottom-nav.tsx
+++ components/bottom-nav.tsx
@@ -58,7 +58,7 @@ export function BottomNav() {
   return (
     <div className="relative">
       <div className="w-full h-[50px] sm:hidden"></div>
-      <nav className="fixed z-[99999] bottom-0 left-0 right-0 border dark:border-border bg-white bg-opacity-60 dark:bg-black dark:bg-opacity-50 backdrop-blur-lg  sm:hidden">
+      <nav className="fixed z-[99999] bottom-0 left-0 right-0 border-t dark:border-border bg-white bg-opacity-60 dark:bg-black dark:bg-opacity-50 backdrop-blur-lg  sm:hidden">
         {name ? (
           <div className="flex justify-around py-2">
             {NAV_ITEMS.map(({ id, href, icon: Icon }) => (
