b471bb8c8fff06c9c18b18046bfe83a044ccd28c
Mo-Jain-back <jainmohit1199@outlook.com>
2025-03-02 12:49:33 +0530

    calendar-bg

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -3,7 +3,7 @@ import MainView from "@/components/MainView";
 
 export default async function Home() {
   return (
-    <div className="scrollbar-hide bg-white/90 dark:bg-black/90 backdrop-blur-xl">
+    <div className="scrollbar-hide h-screen bg-white/90 dark:bg-black/90 backdrop-blur-xl">
       <Header />
       <MainView />
     </div>
