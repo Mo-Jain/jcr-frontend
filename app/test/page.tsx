660ed28b8a8ad82b7963c8bd568c2710d25c82d6
Mo-Jain-back <jainmohit1199@outlook.com>
2025-03-13 16:50:51 +0530

    imporving_loader

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,21 @@
 'use client'
 
-import Loader from '@/components/loader';
-// import ServerLoading from "@/components/server-loading";
+// import Loader from '@/components/loader';
+import ServerLoading from "@/components/server-loading";
+
+import "@theme-toggles/react/css/Within.css"
+import { Within } from "@theme-toggles/react"
+import { useState } from "react";
+
 
 const Page = () => {
+  const [isToggled, setToggle] = useState(false)
   return (
     <div className="flex flex-col z-0 bg-background items-center justify-center w-screen h-screen">
-      {/* <div className="w-32 h-32 -mt-20">
-        <ServerLoading/>
-      </div> */}
-      <Loader/>
+      <div className="w-32 h-32 -mt-20">
+        {/* <ServerLoading/> */}
+      </div>
+      {/* <Loader/> */}
     </div>
   )
 };
--- components/loader.tsx
+++ components/loader.tsx
@@ -1,15 +1,19 @@
 import { cn } from "@/lib/utils";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import React from "react"
 
 const Loader = ({className}:{className?:string}) => {
   return (
     <div>
-      <DotLottieReact
-              src="https://lottie.host/3083f51c-0fdb-4190-8042-64ce26af507e/V6RLCxNsch.lottie"
-              loop
-              autoplay
-              className={cn('w-32 h-32',className)}/>
+      <div className={cn('flex flex-col w-16 h-16 gap-[7%] -rotate-45 m-10',className)}>
+        <div className='flex w-full h-full gap-[7%]'>
+          <div className='w-full h-full bg-blue-300 animate-up-right'></div>
+          <div className='w-full h-full bg-blue-600 animate-down-right'></div>
+        </div>
+        <div className='flex w-full h-full gap-[7%]'>
+          <div className='w-full h-full bg-blue-600 animate-up-left'></div>
+          <div className='w-full h-full bg-blue-300 animate-down-left'></div>
+        </div>
+      </div>
     </div>
   )
 };
